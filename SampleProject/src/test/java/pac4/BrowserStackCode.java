e6277fe80a6abb18e909fa7b82a711472f728d39
buddu <buddu@host.docker.internal>
2025-10-27 14:40:36 +0530

    commited

--- SampleProject/src/test/java/pac4/BrowserStackCode.java
+++ SampleProject/src/test/java/pac4/BrowserStackCode.java
@@ -1,38 +1,38 @@
-package pac4;
-
-import java.net.URL;
-import java.util.HashMap;
-
-import org.openqa.selenium.MutableCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.Test;
-
-public class BrowserStackCode {
-	
-	@Test
-	public void sampleTest()
-	{
-		String username="";
-		String password ="";
-		
-		MutableCapabilities capabilities = new MutableCapabilities();
-		HashMap<String, Object> bstackOptions = new HashMap<String, Object>();
-		capabilities.setCapability("browserName", "Chrome");
-		bstackOptions.put("os", "Windows");
-		bstackOptions.put("osVersion", "10");
-		bstackOptions.put("browserVersion", "120.0");
-		bstackOptions.put("userName", "YOUR_USERNAME");
-		bstackOptions.put("accessKey", "YOUR_ACCESS_KEY");
-		bstackOptions.put("consoleLogs", "info");
-		capabilities.setCapability("bstack:options", bstackOptions);
-
-		RemoteWebDriver driver = new RemoteWebDriver(new URL("", capabilities));
-		
-		driver.get("https://www.google.com/");
-		
-		System.out.println(driver.getTitle());
-		
-		driver.close();
-	}
-
-}
+//package pac4;
+//
+//import java.net.URL;
+//import java.util.HashMap;
+//
+//import org.openqa.selenium.MutableCapabilities;
+//import org.openqa.selenium.remote.RemoteWebDriver;
+//import org.testng.annotations.Test;
+//
+//public class BrowserStackCode {
+//	
+//	@Test
+//	public void sampleTest()
+//	{
+//		String username="";
+//		String password ="";
+//		
+//		MutableCapabilities capabilities = new MutableCapabilities();
+//		HashMap<String, Object> bstackOptions = new HashMap<String, Object>();
+//		capabilities.setCapability("browserName", "Chrome");
+//		bstackOptions.put("os", "Windows");
+//		bstackOptions.put("osVersion", "10");
+//		bstackOptions.put("browserVersion", "120.0");
+//		bstackOptions.put("userName", "YOUR_USERNAME");
+//		bstackOptions.put("accessKey", "YOUR_ACCESS_KEY");
+//		bstackOptions.put("consoleLogs", "info");
+//		capabilities.setCapability("bstack:options", bstackOptions);
+//
+//		RemoteWebDriver driver = new RemoteWebDriver(new URL("", capabilities));
+//		
+//		driver.get("https://www.google.com/");
+//		
+//		System.out.println(driver.getTitle());
+//		
+//		driver.close();
+//	}
+//
+//}
